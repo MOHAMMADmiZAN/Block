57f54d3e174522d2c051bd6fd4f0ff9a44084ad6
mizanavr <mizan@avrhub.com>
2022-06-27 20:04:00 +0600

    "Icon Fixed"

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,9 +1,20 @@
 import AppCss from './assets/css/App.module.css'
 import Header from "../Component/Ui/Header";
+import {CssBaseline} from "@mui/material";
+import {createGlobalStyle} from "styled-components";
+
+
+const GlobalStyle = createGlobalStyle`
+     img{
+       object-fit: none!important;
+     }
+   `
 
 function App() {
     return (
       <div className={AppCss.App}>
+          <GlobalStyle/>
+          <CssBaseline/>
           <Header/>
       </div>
     )
--- src/Component/Ui/Header.jsx
+++ src/Component/Ui/Header.jsx
@@ -1,25 +1,27 @@
 import {Avatar, Box, CssBaseline, Grid} from "@mui/material";
 import styled from "styled-components";
-import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
-import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
-import ReplayOutlinedIcon from '@mui/icons-material/ReplayOutlined';
-import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
-import SearchIcon from '@mui/icons-material/Search';
+import { createGlobalStyle } from 'styled-components'
+
 import MetaMask from '../../App/assets/svg/Icons/metamask-bw-2.svg';
 import User from '../../App/assets/svg/Icons/amir-personal.svg';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faEllipsisVertical } from '@fortawesome/free-solid-svg-icons'
+import LeftIcon from '../../App/assets/svg/Icons/Left.svg';
+import RightIcon from '../../App/assets/svg/Icons/Right.svg';
+import Reload from '../../App/assets/svg/Icons/Reload.svg';
+import HomeIcon from '../../App/assets/svg/Icons/Home.svg';
+// import SearchIcon from '../../App/assets/svg/Icons/search.svg';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faEllipsisVertical} from '@fortawesome/free-solid-svg-icons'
+
 
 
 const Item = styled.button`
   width: 30px;
   height: 30px;
   border-radius: 50%;
-  background: rgba(239, 248, 255, 0.2);
   border: none;
+  background: url("/src/App/assets/svg/Icons/buttonCircel.svg") no-repeat center;
   cursor: pointer;
   margin: 10px;
-  filter: drop-shadow(0px 3px 6px black);
   position: relative;
 `
 const SearchInput = styled.input`
@@ -27,15 +29,23 @@ const SearchInput = styled.input`
   height: 30px;
   border-radius: 20px;
   border: none;
-  background: rgba(239, 248, 255, 0.2);
+  background: rgba(239, 248, 255, 0.1);
   padding: 0 40px;
-  filter: drop-shadow(0px 3px 6px black);
-  margin:  10px;
-  
-  
+  margin: 10px;
+  position: relative;
+
 
   &:focus {
     outline: none;
+  }
+  &:before{
+    content: '';
+    background: url("/src/App/assets/svg/Icons/search.svg");
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    width: 17px;
+    height: 17px;
   }
 
 `
@@ -44,29 +54,34 @@ const positionCenter = {
     top: '50%',
     left: '50%',
     transform: 'translate(-50%, -50%)',
+
+}
+const IconSize = {
+    width: '20px',
+    height: '20px',
 }
 
 function Header() {
 
     return (
         <>
-            <CssBaseline/>
             <Box sx={{bgcolor: 'rgba(239,248,255,0.2)', height: '50px', width: '100%'}}>
                 <Grid container sx={{height: '100%'}}>
                     <Grid item lg={3} xs={3} sm={3} xl={3} md={3}>
-                        <Item><ArrowBackIosIcon sx={{...positionCenter}}/></Item>
-                        <Item><ArrowForwardIosIcon sx={{...positionCenter}}/></Item>
-                        <Item><ReplayOutlinedIcon sx={{...positionCenter}}/></Item>
-                        <Item><HomeOutlinedIcon sx={{...positionCenter}}/></Item>
+                        <Item> <Avatar src={LeftIcon} sx={{...positionCenter,width:'10px',height:'17px'}}/> </Item>
+                        <Item><Avatar src={RightIcon} sx={{...positionCenter,width:'10px',height:'17px'}}/></Item>
+                        <Item><Avatar src={Reload} sx={{...positionCenter,width:'16px',height:'17px'}}/></Item>
+                        <Item><Avatar src={HomeIcon} sx={{...positionCenter,width:'17px',height:'18px'}}/></Item>
                     </Grid>
-                    <Grid item lg={6} xs={6} sm={6} xl={6} md={6} sx={{position:'relative'}}>
+                    <Grid item lg={6} xs={6} sm={6} xl={6} md={6} sx={{position: 'relative'}}>
                         <SearchInput/>
-                        <SearchIcon sx={{position:'absolute',top:'30%',left:'3%'}}/>
+                        {/*<SearchIcon sx={{position: 'absolute', top: '30%', left: '3%'}}/>*/}
                     </Grid>
-                    <Grid item lg={3} xs={3} sm={3} xl={3} md={3} sx={{textAlign:'right'}}>
-                        <Item><Avatar src={MetaMask} alt={`Metamask`} sx={{...positionCenter,width:'20px',height:'20px'}} /></Item>
-                        <Item><Avatar src={User} alt={`User`} sx={{...positionCenter,width:'100%',height:'100%'}} /></Item>
-                        <Item><FontAwesomeIcon icon={faEllipsisVertical}  style={{...positionCenter}}/></Item>
+                    <Grid item lg={3} xs={3} sm={3} xl={3} md={3} sx={{textAlign: 'right'}}>
+                        <Item><Avatar src={MetaMask} alt={`Metamask`}
+                                      sx={{...positionCenter,...IconSize}}/></Item>
+                        <Item><Avatar src={User} alt={`User`} sx={{...positionCenter, width: '100%', height: '100%'}}/></Item>
+                        <Item><FontAwesomeIcon icon={faEllipsisVertical} style={{...positionCenter}}/></Item>
                     </Grid>
                 </Grid>
             </Box>
