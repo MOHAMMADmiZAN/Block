5a77f4cdf7b4e5e7598d98bfe87236fda3a1b4ae
mizanavr <mizan@avrhub.com>
2022-06-27 21:56:42 +0600

    "Input Fix"

--- src/Component/Ui/Header.jsx
+++ src/Component/Ui/Header.jsx
@@ -1,4 +1,4 @@
-import {Avatar, Box,Grid} from "@mui/material";
+import {Avatar, Box, Grid} from "@mui/material";
 import styled from "styled-components";
 import MetaMask from '../../App/assets/svg/Icons/metamask-bw-2.svg';
 import User from '../../App/assets/svg/Icons/amir-personal.svg';
@@ -6,9 +6,9 @@ import LeftIcon from '../../App/assets/svg/Icons/Left.svg';
 import RightIcon from '../../App/assets/svg/Icons/Right.svg';
 import Reload from '../../App/assets/svg/Icons/Reload.svg';
 import HomeIcon from '../../App/assets/svg/Icons/Home.svg';
+import SearchIcon from '../../App/assets/svg/Icons/Search.svg';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import {faEllipsisVertical} from '@fortawesome/free-solid-svg-icons'
-
 
 
 const Item = styled.button`
@@ -26,26 +26,24 @@ const SearchInput = styled.input`
   height: 30px;
   border-radius: 20px;
   border: none;
-  background: linear-gradient(360deg, rgba(239,248,255,0.5) 0%, rgba(17,29,20,0.2) 35%, rgba(239,248,255,0.5) 100%);
-  padding: 0 40px;
+  background: linear-gradient(360deg, rgba(239, 248, 255, 0.5) 0%, rgba(17, 29, 20, 0.2) 50%, rgba(239, 248, 255, 0.3) 100%);
+  padding: 0 30px;
   margin: 10px;
   position: relative;
-
 
   &:focus {
     outline: none;
   }
 
-  &:before {
-    content: '';
-    background: url("/src/App/assets/svg/Icons/search.svg");
+  &:after {
+    content: 'after';
     position: absolute;
-    top: 50%;
-    left: 50%;
-    width: 17px;
-    height: 17px;
-  }
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
 
+  }
 `
 const positionCenter = {
     position: 'absolute',
@@ -54,29 +52,27 @@ const positionCenter = {
     transform: 'translate(-50%, -50%)',
 
 }
-const IconSize = {
-    width: '20px',
-    height: '20px',
-}
 
 function Header() {
 
     return (
         <>
-            <Box sx={{bgcolor: 'rgba(239,248,255,0.2)', height: '50px', width: '100%'}}>
+            <Box sx={{background: 'rgba(239,248,255,0.2)', height: '50px', width: '100%'}}>
                 <Grid container sx={{height: '100%'}}>
                     <Grid item lg={3} xs={3} sm={3} xl={3} md={3}>
-                        <Item> <Avatar src={LeftIcon} sx={{...positionCenter,width:'10px',height:'17px'}}/> </Item>
-                        <Item><Avatar src={RightIcon} sx={{...positionCenter,width:'10px',height:'17px'}}/></Item>
-                        <Item><Avatar src={Reload} sx={{...positionCenter,width:'16px',height:'17px'}}/></Item>
-                        <Item><Avatar src={HomeIcon} sx={{...positionCenter,width:'17px',height:'18px'}}/></Item>
+                        <Item><Avatar src={LeftIcon} sx={{...positionCenter, width: '10px', height: '17px'}}/> </Item>
+                        <Item><Avatar src={RightIcon} sx={{...positionCenter, width: '10px', height: '17px'}}/></Item>
+                        <Item><Avatar src={Reload} sx={{...positionCenter, width: '16px', height: '17px'}}/></Item>
+                        <Item><Avatar src={HomeIcon} sx={{...positionCenter, width: '17px', height: '18px'}}/></Item>
                     </Grid>
-                    <Grid item lg={6} xs={6} sm={6} xl={6} md={6} sx={{position: 'relative'}}>
+                    <Grid item lg={6} xs={6} sm={6} xl={6} md={6} sx={{position:'relative'}}>
                         <SearchInput/>
+                        <Avatar src={SearchIcon} sx={{...positionCenter, width: '20px', height: '20px', position:'absolute',top:'50%',left:"30px"}}/>
+
                     </Grid>
                     <Grid item lg={3} xs={3} sm={3} xl={3} md={3} sx={{textAlign: 'right'}}>
                         <Item><Avatar src={MetaMask} alt={`Metamask`}
-                                      sx={{...positionCenter,...IconSize}}/></Item>
+                                      sx={{...positionCenter, width: '20px', height: '20px'}}/></Item>
                         <Item><Avatar src={User} alt={`User`} sx={{...positionCenter, width: '100%', height: '100%'}}/></Item>
                         <Item><FontAwesomeIcon icon={faEllipsisVertical} style={{...positionCenter}}/></Item>
                     </Grid>
